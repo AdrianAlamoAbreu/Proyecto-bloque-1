d7e88c1afd74d745c833bcfa1c198953e4fd0ad5
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-07-19 12:01:43 +0100

    moving player 5º&-5º

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ import { Player } from "./player.js";
 import { Obstacle } from "./obstacle.js";
 import { Game } from "./game.js";
 
-let mainBoard = document.getElementById('main-board')
-let player = new Player (200, 750, mainBoard) 
-let game = new Game(player)
+let mainBoard = document.getElementById("main-board");
+let player = new Player(200, 750, mainBoard);
+let game = new Game(player);
 
+function start() {
+  let obstacleTimer = setInterval(game.createObstacle, 10000);
 
-
-function start () {
-    let obstacleTimer = setInterval (game.createObstacle, 10000)
-    
-    let collisionTimer = setInterval (game.checkCollision, 100)
+  let collisionTimer = setInterval(game.checkCollision, 100);
 }
 
-player.insertPlayer()
-start ()
+let incline = 0;
+
+window.addEventListener("keydown", function (e) {
+  if (e.key === "a") {
+    incline -= 5;
+    player.sprite.style.transform = `rotate(${incline}deg)`;
+  } else if (e.key === "d") {
+    incline += 5;
+    player.sprite.style.transform = `rotate(${incline}deg)`;
+  }
+});
+
+window.addEventListener("keyup", function (e) {
+  if (e.key === "a" || e.key === "d") {
+    player.sprite.style.transform = `rotate(${incline}deg)`;
+  }
+});
+
+player.insertPlayer();
+start();
